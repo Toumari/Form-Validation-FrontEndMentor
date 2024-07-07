8e17b9484af541ec34858b932a0c264d3633a209
Jack Williams <jack.w141@outlook.com>
2024-07-07 16:41:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const submitBtn = document.querySelector(".submit-btn");
+const toast = document.querySelector(".toast");
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const email = document.querySelector("#email");
+const fieldset = document.querySelector("fieldset");
+const form = document.querySelector("form");
+
+submitBtn.addEventListener("click", (e) => {
+  const isValid = form.checkValidity();
+
+  if (isValid) {
+    toast.classList.add("active");
+    fieldset.disabled = true;
+    toast.innerText = "Form submitted successfully";
+    setTimeout(() => {
+      toast.classList.remove("active");
+      fieldset.disabled = false;
+      form.reset();
+    }, 5000);
+  } else {
+    toast.classList.add("active");
+    toast.innerText = "Please fill out all fields";
+    setTimeout(() => {
+      toast.classList.remove("active");
+    }, 5000);
+  }
+  e.preventDefault();
+});
